c4d5d72e2a4ae15a7fbd4360f04840c8e265456c
Winken Jiang <winken.jiang@gmail.com>
2012-03-19 17:46:28 -0400

    Signed-off-by: Winken Jiang <winken.jiang@gmail.com>

--- src/com/gg/SIPVActivity.java
+++ src/com/gg/SIPVActivity.java
@@ -5,6 +5,7 @@ package com.gg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class SIPVActivity extends Activity {
     /** Called when the activity is first created. */
@@ -12,5 +13,12 @@ public class SIPVActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+    	boolean result = super.onCreateOptionsMenu(menu);
+    	
+    	return result;
     }
 }
